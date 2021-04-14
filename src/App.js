0eb61284d2c6a0ed61da6155c504600d01d2abda
Oscar Cabanillas <oskbanillas@gmail.com>
2021-04-14 10:17:36 -0300

    Se agrego la logica para obtener los datos y mostrarla en el componenete FRASE

--- src/App.js
+++ src/App.js
@@ -15,17 +15,22 @@ function App() {
   }, []); // se ejecuta solo en estapa de montaje)
 
   // comienzo a utilizar la Api
+  //async es para cuando tengo que esperar una respuesta
+  // va de la mano con await
   const consultarApi = async () => {
     const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
-    console.log(respuesta);
+    const resultado = await respuesta.json()
+    console.log(resultado[0]);
+    // guardar los datos en el state
+    setPersonaje(resultado[0]);
   }
 
   return (
     <section className='container my-5 d-flex flex-column align-items-center'>
       {/* process.env.PUBLIC_URL este obejto me devuelva la direccion de la carpeta de mi proyecto */}
       <img src={process.env.PUBLIC_URL +'logo.png'} alt="logo simpsons" className='w-75 my-4'/>
-      <Button variant="warning">Obtener Frase</Button>
-      <Frase />
+      <Button variant="warning" onClick={() => consultarApi()}>Obtener Frase</Button>
+      <Frase personaje={personaje}/>
     </section>
   );
 }
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -4,18 +4,18 @@ import Card from "react-bootstrap/Card";
 //Opcion para importar varios cosas
 //import { Card, Button } from "react-bootstrap";
 
-const Frase = () => {
+const Frase = (props) => {
   return (
-    <Card className='w-100 my-5'>
+    <Card className="w-100 my-5">
       <Card.Body>
         <div className="row">
           <div className="col-sm-12 col-md-4">
-            <img src="" alt=""/>
+            <img src={props.personaje.image} alt={props.personaje.character} />
           </div>
           <div className="col-sm-12 col-md-8">
-            <Card.Title>Personaje</Card.Title>
-             <Card.Text>
-                 Frase del personaje
+            <Card.Title>{props.personaje.character}</Card.Title>
+            <Card.Text>
+              <h3>{props.personaje.quote}</h3>
             </Card.Text>
           </div>
         </div>
